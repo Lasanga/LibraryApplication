e67044ee17b40bc1663c3e8bacd9a8c3b6059a4d
minulmindula <minul.mindula72@gmail.com>
2019-04-15 17:44:41 +0530

    Card

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -7,17 +7,27 @@ import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BooksService, CategoryService } from './shared-services/shared-services.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+//Material Component Imports
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [BooksService, CategoryService],
   bootstrap: [AppComponent]
