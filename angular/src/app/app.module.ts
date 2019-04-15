944dc5d0cbe76a79e6537e49ba4c117d476e41e8
minulmindula <minul.mindula72@gmail.com>
2019-04-15 15:04:28 +0530

    Header component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BooksService, CategoryService } from './shared-services/shared-services.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
